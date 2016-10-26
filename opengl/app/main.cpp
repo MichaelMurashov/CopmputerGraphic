1d69908c704b6a6a922dcba19abd296dcea2c307
MichaelMurashov <mmv047@gmail.com>
2016-10-26 22:56:16 +0300

    add sample

---
+++ opengl/app/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by michaelmurashov on 26.10.16.
+//
+
+#include "GL/glut.h"
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("opengl");
+
+    return 0;
+}
