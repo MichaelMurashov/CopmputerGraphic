abd3b9ee26d11d5f1e9c83a3b8bfeae3026b41de
Michael Murashov <mmv047@gmail.com>
2017-05-04 21:23:42 +0300

    add range

--- Segmentation/main.cpp
+++ Segmentation/main.cpp
@@ -10,7 +10,7 @@ int getIntensity(const Vec3b& color)  {
 }
 
 int check(const vector<vector<Vec3b>>& segments, Vec3b pixel) {
-    Vec3b range(10, 10, 10);
+    Vec3b range(2, 2, 2);
 
     for (int i = 0; i < segments.size(); i++) {
         Vec3b threshold = segments[i].operator[](0);
@@ -19,11 +19,8 @@ int check(const vector<vector<Vec3b>>& segments, Vec3b pixel) {
         int border2 = getIntensity(threshold + range);
         int src = getIntensity(pixel);
 
-        if (border1 < src && src < border2)
+        if (border1 <= src && src <= border2)
             return i;
-
-//        if (threshold - range < pixel && pixel < threshold + range)
-//            return i;
     }
 
     return -1;
@@ -38,14 +35,12 @@ int clamp(int value, int min, int max) {
 }
 
 int main() {
-    const Mat src = imread("../images/src.png");
+    const Mat src = imread("../images/qwe.png");
 //    imshow("src", src);
 
     vector<vector<Vec3b>> segments;
 
     for (int i = 0; i < src.rows; i++) {
-        cout << i << " ";
-
         for (int j = 0; j < src.cols; j++) {
             Vec3b pixel = src.at<Vec3b>(i, j);
             int pos = check(segments, pixel);
@@ -61,24 +56,24 @@ int main() {
         }
     }
 
-//    vector<int> perimeters;
-//    for (int i = 0; i < segments.size(); i++) {
-//        perimeters.push_back(0);
-//
-//        for (int x = 1; x < src.rows - 1; x++)
-//            for (int y = 1; y < src.cols - 1; y++) {
-//                Vec3b source = segments[i].operator[](0);
-//                Vec3b intensity = src.at<Vec3b>(x, y);
-//
-//                Vec3b left  = src.at<Vec3b>(x - 1, y);
-//                Vec3b up    = src.at<Vec3b>(x,     y + 1);
-//                Vec3b right = src.at<Vec3b>(x + 1, y);
-//                Vec3b down  = src.at<Vec3b>(x,     y - 1);
-//
-//                if (intensity == source && (left != source || up != source || right != source || down != source))
-//                    perimeters[i]++;
-//            }
-//    }
+    vector<int> perimeters;
+    for (int i = 0; i < segments.size(); i++) {
+        perimeters.push_back(0);
+
+        for (int x = 1; x < src.rows - 1; x++)
+            for (int y = 1; y < src.cols - 1; y++) {
+                Vec3b source = segments[i].operator[](0);
+                Vec3b intensity = src.at<Vec3b>(x, y);
+
+                Vec3b left  = src.at<Vec3b>(x - 1, y);
+                Vec3b up    = src.at<Vec3b>(x,     y + 1);
+                Vec3b right = src.at<Vec3b>(x + 1, y);
+                Vec3b down  = src.at<Vec3b>(x,     y - 1);
+
+                if (intensity == source && (left != source || up != source || right != source || down != source))
+                    perimeters[i]++;
+            }
+    }
 
     cout << endl << "Размер изображения: " << src.size() << endl
          << "Кол-во сегментов - " << segments.size() << endl
@@ -87,9 +82,9 @@ int main() {
     for (int i = 0; i < segments.size(); i++)
         cout << "   сегмент №" << i << endl
              << "       цвет - " << segments[i].operator[](0) << endl
-             << "       площадь - " << segments[i].size() << endl;
-            // << "       периметр - " << perimeters[i] << endl
-            // << "       компактность - " << (perimeters[i] * perimeters[i]) / segments[i].size() << endl;
+             << "       площадь - " << segments[i].size() << endl
+             << "       периметр - " << perimeters[i] << endl
+             << "       компактность - " << (perimeters[i] * perimeters[i]) / segments[i].size() << endl;
 
 //    waitKey(0);
     return 0;
